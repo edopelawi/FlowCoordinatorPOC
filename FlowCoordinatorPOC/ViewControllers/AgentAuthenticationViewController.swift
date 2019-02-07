d31b1d8b730666ebaa7a854ac30b28ec5c5a0581
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 07:14:35 +0700

    Initial commit for AgentAuthenticationViewController

---
+++ FlowCoordinatorPOC/ViewControllers/AgentAuthenticationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AgentAuthenticationViewController.swift
+//  FlowCoordinatorPOC
+//
+//  Created by Ricardo Pramana Suranta on 07/02/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class AgentAuthenticationViewController: UIViewController {
+
+	@IBOutlet weak var inputTextField: UITextField!
+
+	@IBAction func authenticateAgent(_ sender: Any) {
+		// TODO: Implement this later.
+	}
+}
