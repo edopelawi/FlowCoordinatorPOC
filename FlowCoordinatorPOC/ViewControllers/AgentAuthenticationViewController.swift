410b5adf4435a64a651205b3259101e0f1ec5fcd
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 11:28:43 +0700

    Configure navigation bar display in AgentAuthenticationViewController

--- FlowCoordinatorPOC/ViewControllers/AgentAuthenticationViewController.swift
+++ FlowCoordinatorPOC/ViewControllers/AgentAuthenticationViewController.swift
@@ -12,6 +12,13 @@ final class AgentAuthenticationViewController: UIViewController {
 
 	@IBOutlet weak var inputTextField: UITextField!
 
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		navigationController?.isNavigationBarHidden = true
+		navigationItem.largeTitleDisplayMode = .never
+	}
+
 	@IBAction func authenticateAgent(_ sender: Any) {
 		// TODO: Implement this later.
 	}
