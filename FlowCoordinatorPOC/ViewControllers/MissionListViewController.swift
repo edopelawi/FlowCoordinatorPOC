60534c6f4b443728ce207191ddc89abc434a7319
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 11:10:05 +0700

    Initial commit for MissionListViewController

---
+++ FlowCoordinatorPOC/ViewControllers/MissionListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MissionListViewController.swift
+//  FlowCoordinatorPOC
+//
+//  Created by Ricardo Pramana Suranta on 07/02/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class MissionListViewController: UIViewController {
+
+	@IBOutlet weak var tableView: UITableView!
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+extension MissionListViewController: UITableViewDataSource {
+
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+
+}
