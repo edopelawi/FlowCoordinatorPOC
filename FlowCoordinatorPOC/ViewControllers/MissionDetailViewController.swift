1ce0381f49ea82ecaea726bc7722ba0becf793b0
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 11:20:50 +0700

    Initial commit for MissionDetailViewController

---
+++ FlowCoordinatorPOC/ViewControllers/MissionDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MissionDetailViewController.swift
+//  FlowCoordinatorPOC
+//
+//  Created by Ricardo Pramana Suranta on 07/02/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class MissionDetailViewController: UIViewController {
+
+	@IBOutlet weak var descriptionTextView: UITextView!
+	@IBOutlet weak var actionButton: UIButton!
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+	@IBAction func executeAction(_ sender: Any) {
+	}
+
+}
