b3ea898e34858980f1216261c86c8ba1f840911e
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 12:45:06 +0700

    Add finished parameter to Mission

--- FlowCoordinatorPOC/Entities/Mission.swift
+++ FlowCoordinatorPOC/Entities/Mission.swift
@@ -11,15 +11,18 @@ import Foundation
 struct Mission {
 	let name: String
 	let description: String
+	let finished: Bool
 }
 
 extension Mission {
 
-	init(name: String) {
+	/// Creates Mission with default description.
+	init(name: String, finished: Bool) {
 
 		self.init(
 			name: name,
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+			finished: finished
 		)
 	}
 }
