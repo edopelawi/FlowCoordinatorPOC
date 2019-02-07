34b2ab67b0f42a79b929c356c967a33b77989c07
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 12:41:27 +0700

    Initial commit for Agent entity

---
+++ FlowCoordinatorPOC/Entities/Agent.swift
@@ -0,0 +1,14 @@
+//
+//  Agent.swift
+//  FlowCoordinatorPOC
+//
+//  Created by Ricardo Pramana Suranta on 07/02/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+struct Agent {
+	let codename: String
+	let secretPIN: String
+}
