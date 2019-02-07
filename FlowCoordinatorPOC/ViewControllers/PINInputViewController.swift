706132c6f6c58bde039051352fd3b6f041f70618
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-02-07 11:00:18 +0700

    Initial commit for PINInputViewController

---
+++ FlowCoordinatorPOC/ViewControllers/PINInputViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PINInputViewController.swift
+//  FlowCoordinatorPOC
+//
+//  Created by Ricardo Pramana Suranta on 07/02/19.
+//  Copyright © 2019 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+final class PINInputViewController: UIViewController {
+
+	@IBOutlet weak var inputTextField: UITextField!
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+	@IBAction func inputChanged(_ sender: Any) {
+		// TODO: Add code to navigate to next page after four input later on.
+	}
+}
